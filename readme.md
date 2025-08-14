c5f9b0096a87513f4c5f6cbabb90e1377512a96b
Jess Stevenson <jws168@txstate.edu>
2025-08-14 17:32:26 -0500

    current weatrher data scraping and display

--- readme.md
+++ readme.md
@@ -5,3 +5,6 @@ https://realpython.com/beautiful-soup-web-scraper-python/#step-3-parse-html-code
 https://realpython.com/beautiful-soup-web-scraper-python/#step-2-scrape-html-content-from-a-page
 
 https://www.geeksforgeeks.org/python/implementing-web-scraping-python-beautiful-soup/#
+
+weather service:
+https://www.timeanddate.com/weather/@z-us-77954/hourly
--- weatherScrapeV2.py
+++ weatherScrapeV2.py
@@ -9,15 +9,33 @@ soup = bs(page.content, 'html.parser')
 
 graph = soup.find(id="wt-hbh")
 times = graph.find_all("tr")
-data = times[2].find("th").get_text("\n")
+
+dateTime = times[2].find("th").get_text(" on ")  #date and time
+data = times[2].find_all("td")  #breaks current data into chunks
+
+temp = data[1].get_text()   #gets temp
+
+weather = data[2].get_text()    #gets weather
+weather = weather.removesuffix('.') 
+
+feels = data[3].get_text()  #what it feels like outside
+
+wind = data[4].get_text()   #wind
+
+humid = data[6].get_text()  #humiidity
+
+rain = data[7].get_text() #rain chance
+
+chance = data[8]
+
+print('')
+print('date: '+dateTime)
+print('temp: '+temp)
+print('weather: '+ weather)
+print('feels like: '+feels)
+print('wind: '+wind)
+print('humidity: '+ humid)
+print('chance of rain: ' + rain)
 
 
-
-for slice in times:
-    print(slice.text)
-    print()
-
-print('\n\n\n')
-print(data)
-print('\n\n\n')
 
