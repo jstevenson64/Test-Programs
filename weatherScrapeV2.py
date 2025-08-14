b1d2a7bf3d4c9b90aa9ec1378f78ba7375549e06
Jess Stevenson <jws168@txstate.edu>
2025-08-14 17:09:08 -0500

    printed out date and time

--- weatherScrapeV2.py
+++ weatherScrapeV2.py
@@ -2,22 +2,22 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-url = "https://www.timeanddate.com/weather/@z-us-77954"
+url = "https://www.timeanddate.com/weather/@z-us-77954/hourly"
 page = requests.get(url)
 soup = bs(page.content, 'html.parser')
 #print(soup.prettify())
-waag = soup.find(id="qlook")
-cast = soup.find("div", class_="eight columns")
-#print(waag.prettify())
 
-temp= waag.find("div",class_="h2")
-para = waag.find_all("p")
-feels = para[1].find_all()
-like = feels
+graph = soup.find(id="wt-hbh")
+times = graph.find_all("tr")
+data = times[2].find("th").get_text("\n")
 
-print("The weather in Cuero today is: ")
-print(temp.text)
-print(feels[1].text.strip())
-print(para[0].text.strip())
 
+
+for slice in times:
+    print(slice.text)
+    print()
+
+print('\n\n\n')
+print(data)
+print('\n\n\n')
 
